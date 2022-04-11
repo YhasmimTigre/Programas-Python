3fe3ae9e5263f66f5c30865e9f4950e4ebe0003f
Yhasmim <93266317+YhasmimTigre@users.noreply.github.com>
2022-04-11 10:48:24 -0300

    Update area_de_um_circulo_yhasmim_tigre.py

--- area_de_um_circulo_yhasmim_tigre.py
+++ area_de_um_circulo_yhasmim_tigre.py
@@ -1,8 +1,9 @@
-
-
+#Vamos descobrir o raio de um circulo. O valor do Raio é inputado pelo usuário
 print ("insira raio do circulo")
 raio = input()
+#tornamos o valor de raio em float
 raio = float (raio)
-a = 3.14 * raio*raio
+#a é variável para area. multiplicamos o valor de pi pelo raio² 
+a = 3.14 * raio * raio
 print("area de um circulo:", a)
 input()
