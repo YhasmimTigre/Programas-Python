4e86087cc5b95f1cdeb74ee0c9144a2b7a5bd8a0
Yhasmim <93266317+YhasmimTigre@users.noreply.github.com>
2022-04-16 09:00:08 -0300

    Add files via upload

---
+++ media_conceitos_yhasmim_tigre.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Sep 23 18:02:52 2021
+
+@author: Yhasmim
+"""
+
+print("Digite sua primeira nota")
+prim_nota = input()
+prim_nota = float(prim_nota)
+print("Digite sua segunda nota nota")
+seg_nota = input()
+seg_nota = float(seg_nota)
+media=(prim_nota + seg_nota)/2
+if (media >= 9.0 and media < 10.0):
+    print("conceito A")
+elif(media >= 7.5 and media < 9.0):
+    print("Conceito B")
+elif(media >= 6.0 and media < 7.5):
+    print("Conceito C")
+elif (media >= 4.0 and media < 6.0):
+    print("Conceito D")
+elif (media >= 0 and media < 4.0):
+    print("Conceito E")
+
+input()
