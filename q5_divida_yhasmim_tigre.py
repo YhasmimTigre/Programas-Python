c88fcceedf94ec792443f4ff2e9cbfc7dd84081b
Yhasmim <93266317+YhasmimTigre@users.noreply.github.com>
2022-04-28 09:44:08 -0300

    Add files via upload

---
+++ q5_divida_yhasmim_tigre.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Sep 29 22:20:40 2021
+
+@author: Yhasmim
+"""
+
+print("qual o valor inicial da divida?")
+vi = input()
+vi = float(vi)
+print("qual o juros mensal?")
+jm = input() 
+jm = float(jm)
+print("qual o valor mensal que será pago?")
+vm = input() 
+vm = float(vm)
+valor_inicial = vi
+
+meses = 0
+total_juros = 0
+while (vi >= vm) :
+        juros = vi*(jm/100)
+        vi = vi + (juros - vm)
+        total_juros = total_juros + juros
+        meses = meses + 1
+
+total_pago = valor_inicial + total_juros 
+print(f"total de meses:{meses}")
+print(f"total de juros:{total_juros:.2f}")
+print(f"total da divida:{total_pago:.2f}")
+
+input() 
