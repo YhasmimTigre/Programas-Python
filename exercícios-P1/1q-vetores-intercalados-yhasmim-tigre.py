c73e6a48d1e1aab63405354938179cab4c1a631c
Yhasmim <93266317+YhasmimTigre@users.noreply.github.com>
2022-07-11 11:35:18 -0300

    Add files via upload

---
+++ exercícios-P1/1q-vetores-intercalados-yhasmim-tigre.py
@@ -0,0 +1,26 @@
+"""
+Created on Sat Oct 30 15:30:49 2021
+
+@author: Yhasmim
+"""
+
+#definimos as listas
+
+vetor_x = []
+vetor_y = []
+
+#criamos o input para o usuário
+
+for i in range(0,5):
+    vetor_x.append(int(input("digite os valores para o vetor 1: ")))
+
+for i in range(0,5):
+    vetor_y.append(int(input("digite os valores para o vetor 2: ")))
+
+#importamos chain de intertools para fazer as listas interagirem
+
+from itertools import chain
+
+resultado = chain.from_iterable(zip(vetor_x, vetor_y))
+
+print("União intercalada dos vetores 1 e 2:", *resultado)
